c451f1791450c91cef65fd2e039c9a5038caf15b
dbwofla11 <101052855+dbwofla11@users.noreply.github.com>
2022-07-29 14:59:56 +0900

    gg

--- 스크랩핑연습/kakakoCR.py
+++ 스크랩핑연습/kakakoCR.py
@@ -1,8 +1,15 @@
 # 숭실대 학사 공지사항 가지고 오기 
 import requests
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
+import re
 
-num = 2 # 2페이지  
+count = 0 # 진행중인거 파밍한거 
+num = 2 # 2페이지  ~ 5 페이지 
+status_list = []
+t_list = []
+l_list = []
+ele_list = []
+
 url = "https://scatch.ssu.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/?f&category=%ED%95%99%EC%82%AC&keyword"
 url2 = "https://scatch.ssu.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/page/{}/?f&category=%ED%95%99%EC%82%AC&keyword".format(num) # 2번째 페이지 
 myheaders = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.49"}
@@ -11,17 +18,41 @@ res = requests.get(url , headers=myheaders)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text , "lxml")
-tags = soup.find_all("span", attrs={"class" : "tag ing"})
+# 일단 테스트로 1페이지만 
 
-title = soup.find_all("div" , attrs={"class" : "row no-gutters align-items-center"} )
-
-status = []
-for tag in tags:
-    status.append(tag.get_text()) 
-# 인덱스 0 부터 시작하는 진행도 상태  # 상태가 진행이여야만 웹 스크랩핑이 가능하게끔 할거임 
-# ["진행" , "진행" , "진행" , "진행" , "진행"] # 이렇게 진행인 a 태그만 스크랩핑 설계
-
-# rank = s
+notilist = soup.find("ul" , attrs = {"class" : "notice-lists"}).find_all("li")
 
 
-# def best_notice():
+for li in notilist:
+    listname = li["class"]
+    if listname == "notice_head":
+        print("여기는 헤드입니다")
+    else:
+        status = li.select_one(".notice_col2")
+        if status.span == None:
+            status_list.append("None")
+            pass
+        else:
+            status_list.append(status.span.string)
+# print(status_list)
+# string 과 get_text()는 약간 다름 전자는 span에 후자는 a태크에 씀 
+
+for a in notilist:
+    notice = a.select_one(".notice_col3")
+    if notice.a == None:
+        t_list.append("None1")
+        l_list.append("None")
+        pass
+    else:
+        title = notice.a.get_text()
+        link = notice.a["href"]
+
+        t_list.append(title)
+        l_list.append(link)
+
+# 필터적용 적용된거는 DB에 넣을거임 dic에 안넣음 보류 
+for i in len(notilist):
+
+
+
+            
